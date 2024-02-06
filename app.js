bdbc9dc9511f9a7ffe0a3dae132f88aa949b877f
Sergey Роднуля <116501676+Qvanchik57@users.noreply.github.com>
2024-02-06 14:18:00 +0300

    Update app.js
    fix limiter, messageError, constants

--- app.js
+++ app.js
@@ -3,31 +3,28 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const { limiter } = require('./middlewares/rateLimit');
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/notFoundError');
+const { mongoDbAddr, errorMessage, statusError } = require('./utils/constants');
 
-const { PORT = 3000 } = process.env;
+const { PORT, MONGO_DB, NODE_ENV } = process.env;
 
 const app = express();
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100 // можно совершить максимум 100 запросов с одного IP
-});
 
-const SERVER_ERROR = 500;
+const SERVER_ERROR = statusError.serverError;
 
-app.use(limiter); 
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 
-mongoose.connect('mongodb://localhost:27017/mestodb')
+mongoose.connect(NODE_ENV === 'production' ? MONGO_DB : mongoDbAddr.mongoDB)
   .then(() => console.log('connected'))
   .catch((err) => console.log(`Произошло ошибка ${err.name}: ${err.message}`));
 
@@ -35,7 +32,7 @@ app.use(requestLogger);
 
 app.use(routes);
 app.use('/', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
+  next(new NotFoundError(errorMessage.notFoundPageErrorMessage));
 });
 
 app.use(errorLogger);
@@ -46,7 +43,7 @@ app.use((err, req, res, next) => {
   const { statusCode = SERVER_ERROR, message } = err;
   res.status(statusCode).send({
     message: statusCode === SERVER_ERROR
-      ? 'На сервере произошла ошибка'
+      ? errorMessage.serverError
       : message,
   });
   next();
