ccd8d84957ca56465d2545edaf52d63749125215
Sergey Роднуля <116501676+Qvanchik57@users.noreply.github.com>
2024-02-06 14:45:06 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken');
 const Users = require('../models/user');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-
-const GOOD_REQ = 200;
-const CREATE_REQ = 201;
-
 const ValidationError = require('../errors/validationError');
 const NotFoundError = require('../errors/notFoundError');
 const ConflictError = require('../errors/conflictError');
+const { statusReq, errorMessage, statusError } = require('../utils/constants');
+
+const GOOD_REQ = statusReq.goodReq;
+const CREATE_REQ = statusReq.createReq;
 
 module.exports.getThisUser = (req, res, next) => {
   Users.findById(req.user._id)
@@ -32,25 +32,25 @@ module.exports.patchUser = async (req, res, next) => {
   )
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Пользователь с указанным _id не найден');
+        throw new NotFoundError(errorMessage.notFoundUserError);
       }
       res.status(GOOD_REQ).send(user);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new ValidationError('Переданы некорректные данные при обновлении профиля'));
+        next(new ValidationError(errorMessage.validationPatchError));
       } else {
         next(err);
       }
     });
 };
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = async (req, res, next) => {
   const {
     email,
     name,
   } = req.body;
-  bcrypt.hash(req.body.password, 10)
+  await bcrypt.hash(req.body.password, 10)
     .then((hash) => Users.create({
       email,
       password: hash,
@@ -61,19 +61,19 @@ module.exports.createUser = (req, res, next) => {
     }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new ValidationError('Переданы некорректные данные при создании пользователя'));
-      } if (err.code === 11000) {
-        next(new ConflictError('Пользователь уже зарегистрирован'));
+        next(new ValidationError(errorMessage.validationCreateUserError));
+      } if (err.code === statusError.conflictError) {
+        next(new ConflictError(errorMessage.conflictError));
       } else {
         next(err);
       }
     });
 };
 
-module.exports.login = (req, res, next) => {
+module.exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  return Users.findUserByCredentials(email, password)
+  return await Users.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
